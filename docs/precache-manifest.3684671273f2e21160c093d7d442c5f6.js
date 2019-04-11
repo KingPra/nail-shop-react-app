40c2a065c6a07a62642f683b86eb8f4ce3674091
King Pra <kingi.pra@gmail.com>
2019-04-11 13:47:53 -0400

    rename build folder to docs

---
+++ docs/precache-manifest.3684671273f2e21160c093d7d442c5f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5fab2574ff7b0e568933",
+    "url": "/static/js/main.b264baf5.chunk.js"
+  },
+  {
+    "revision": "0394893f9ea6d8099f7e",
+    "url": "/static/js/2.4f4cd2a1.chunk.js"
+  },
+  {
+    "revision": "5fab2574ff7b0e568933",
+    "url": "/static/css/main.eda5bf8b.chunk.css"
+  },
+  {
+    "revision": "a92f2c80179511be38ed4c7ed76852da",
+    "url": "/index.html"
+  }
+];
